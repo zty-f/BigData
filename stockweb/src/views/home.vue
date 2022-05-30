b7676148b00e170daf76ab4f055c0360b599a3a8
zty-f <2559247260@qq.com>
2022-05-30 22:06:15 +0800

    2022.5.30 22:05 功能基本实现（小bug：点击图表重复请求）

--- stockweb/src/views/home.vue
+++ stockweb/src/views/home.vue
@@ -23,11 +23,20 @@
         </ul>
       </div>
     </el-col>
-
     <el-col :span="17">
-      <div id="main" style="width: 100%;height: 400px;"></div>
+      <div id="main" style="width: 100%;height: 550px;"></div>
       <el-col>
-
+        <el-table :data="everyDayData" style="width: 100%">
+          <el-table-column prop="code" label="股票代码" width="100px"/>
+          <el-table-column prop="open" label="开盘价" />
+          <el-table-column prop="close" label="收盘价" />
+          <el-table-column prop="high" label="最高价" />
+          <el-table-column prop="low" label="最低价" />
+          <el-table-column prop="volume" label="交易量(手)" width="100px"/>
+          <el-table-column prop="money" label="交易量(元)" width="1300px"/>
+          <el-table-column prop="paused" label="是否停牌" />
+          <el-table-column prop="st" label="是否st" />
+        </el-table>
       </el-col>
     </el-col>
   </el-row>
@@ -39,11 +48,11 @@ import {reactive, ref} from 'vue';
 import axios from "axios";
 import {ElMessage} from 'element-plus';
 import * as echarts from 'echarts';
+import {ECharts} from "echarts";
 
 //声明Echarts
 const input = ref('')
 const keyword=ref("6006");
-const curDate=ref("2022-01-05");
 
 
 const stocks = reactive([{
@@ -103,6 +112,7 @@ const selectOne = (code: String,name:String) => {
   return;
 }
 
+
 const getStockPrice = (code: String,name:String) => {
   console.log('获取股票价格信息······');
   axios.get("/GetStockPrice?code="+code).then((response) => {
@@ -114,7 +124,7 @@ const getStockPrice = (code: String,name:String) => {
       }
       console.log(prices);
       ElMessage.success("获取股票价格信息成功！");
-      render(name);
+      render(code,name);
     } else {
       ElMessage.error(data.msg);
     }
@@ -122,8 +132,8 @@ const getStockPrice = (code: String,name:String) => {
   return;
 }
 
-const render = (name:String) => {
-  // 基于准备好的dom，初始化echarts实例
+const render = (code: String,name:String) => {
+
   const myChart = echarts.init(document.getElementById("main") as HTMLElement);
 
   // 指定图表的配置项和数据
@@ -157,19 +167,34 @@ const render = (name:String) => {
     option.series[0].data[i] = parseFloat(prices[i].price);
   }
   // 使用刚指定的配置项和数据显示图表。
-  myChart.setOption(option);
+  myChart.setOption(option,true);
+  //设置图表点击交互事件
+  myChart.on("click", function (params) {
+    console.log("点击图表······");
+    console.log("当前股票代码为："+code);
+    console.log("当前选中日期为"+prices[params.dataIndex].date);
+    getStockData(code,prices[params.dataIndex].date);
+  })
   return;
 }
 
 
-const getStockData = (curCode:String) => {
+
+const getStockData = (curCode:String,curDate:String) => {
   console.log('获取股票每日详细信息············');
   axios.get("/GetStockData?code="+curCode+"&date="+curDate).then((response) => {
     const data = response.data;
     if (data.state == "SUC") {
-      for (let i = 0; i < data.content.length; i++) {
-        everyDayData.push(data.content[i]);
-      }
+      everyDayData[0].code = data.content.code;
+      everyDayData[0].volume = data.content.volume;
+      everyDayData[0].low = data.content.low;
+      everyDayData[0].high = data.content.high;
+      everyDayData[0].st = data.content.st;
+      everyDayData[0].close = data.content.close;
+      everyDayData[0].money = data.content.money;
+      everyDayData[0].paused = data.content.paused;
+      everyDayData[0].open = data.content.open;
+      console.log(everyDayData);
       ElMessage.success("获取股票每日详细信息成功！");
     } else {
       ElMessage.error(data.msg);
